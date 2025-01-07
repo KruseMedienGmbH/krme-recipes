eb336727fe7917af12ca5d8c0352d671f38c88d5
Thomas Buning <tb@krusemedien.com>
2025-01-07 10:21:52 +0100

    chore: update version 6.6.0

--- krme/code-quality-meta/6.6.0/vendor-bin/rector/custom_rector.php
+++ krme/code-quality-meta/6.6.0/vendor-bin/rector/custom_rector.php
@@ -1,63 +1,54 @@
 <?php
+// rector.php
 
 declare(strict_types=1);
 
-/*
- * (c) KruseMedien GmbH <info@krusemedien.com>
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
+use Frosh\Rector\Rule\ClassConstructor\RemoveArgumentFromClassConstructRector;
+use Frosh\Rector\Rule\ClassMethod\AddArgumentToClassWithoutDefaultRector;
 use Frosh\Rector\Set\ShopwareSetList;
+use Rector\CodingStyle\Rector\String_\SymplifyQuoteEscapeRector;
+use Rector\DeadCode\Rector\Cast\RecastingRemovalRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
+use Rector\DeadCode\Rector\StaticCall\RemoveParentCallWithoutParentRector;
+use Rector\Privatization\Rector\ClassMethod\PrivatizeFinalClassMethodRector;
+use Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector;
+use Rector\Privatization\Rector\Property\PrivatizeFinalClassPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\ValueObject\PhpVersion;
-use Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector;
-use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
-use Rector\Set\ValueObject\SetList;
-use Rector\Symfony\Set\SymfonySetList;
-use Rector\Symfony\Set\TwigSetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/../../ecs.php',
-        __DIR__ . '/../../rector.php',
-        __DIR__ . '/../../src',
-    ]);
 
-    $rectorConfig->skip([]);
-
-    $rectorConfig->sets([
-        SymfonySetList::SYMFONY_52,
-        SymfonySetList::SYMFONY_53,
-        SymfonySetList::SYMFONY_54,
-        SymfonySetList::SYMFONY_CODE_QUALITY,
-        TwigSetList::TWIG_240,
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::NAMING,
-        SetList::DEAD_CODE,
-        SetList::PHP_72,
-        SetList::PHP_73,
-        SetList::PHP_74,
-        SetList::PHP_80,
-        SetList::PHP_81,
-        SetList::PHP_82,
-        SetList::TYPE_DECLARATION,
-        SetList::PRIVATIZATION,
+return RectorConfig::configure()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        privatization: true,
+        naming: true,
+        instanceOf: true,
+        strictBooleans: true,
+        symfonyCodeQuality: true
+    )
+    ->withSymfonyContainerPhp(__DIR__ . '/tests/RectorSymfonyContainerBootstrap.php')
+    ->withSymfonyContainerXml(__DIR__ . '/../../../var/cache/phpstan_dev/Shopware_Core_DevOps_StaticAnalyze_StaticAnalyzeKernelPhpstan_devDebugContainer.xml')
+    ->withPaths([
+        __DIR__ . '/src',
+    ])
+    ->withSkip(
+        []
+    )
+    ->withBootstrapFiles([
+        __DIR__ . '/tests/PHPStanBootstrap.php',
+    ])
+    ->withPhpVersion(PhpVersion::PHP_82)
+    ->withAutoloadPaths([
+        __DIR__ . '/../../../vendor/store.shopware.com',
+    ])
+    ->withPhpSets(
+        php83: true
+    )
+    ->withSets([
         ShopwareSetList::SHOPWARE_6_6_0,
         ShopwareSetList::SHOPWARE_6_5_0,
-    ]);
-
-    $rectorConfig->phpVersion(PhpVersion::PHP_82);
-
-    $rectorConfig->skip([
-        AddLiteralSeparatorToNumberRector::class => [
-            __DIR__ . '/src/Migration/Migration*.php',
-        ],
-        RenamePropertyToMatchTypeRector::class => [
-            __DIR__ . '/src/**/Subscriber/*Subscriber.php',
-        ],
-    ]);
-
-    $rectorConfig->parallel();
-};
+    ])
+    ->withParallel();
