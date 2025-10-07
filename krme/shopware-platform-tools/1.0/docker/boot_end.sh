e516ba23b9b5388d7828dd2efa53867a0f50113b
Thomas Buning <tb@krusemedien.com>
2025-10-07 09:37:06 +0200

    chore: optimize dockerfile

--- krme/shopware-platform-tools/1.0/docker/boot_end.sh
+++ krme/shopware-platform-tools/1.0/docker/boot_end.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 vendor/bin/platform-tools misc:install_dev_tools
+
+sed -i s/APP_ENV=prod/APP_ENV=dev/g /var/www/html/.env
--- krme/shopware-platform-tools/1.0/docker/boot_start.sh
+++ krme/shopware-platform-tools/1.0/docker/boot_start.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+export NVM_DIR="/var/www/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
