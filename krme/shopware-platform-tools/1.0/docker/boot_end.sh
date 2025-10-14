c582ea59f64166adf0ea5e2c1421f752526c3f65
Thomas Buning <tb@krusemedien.com>
2025-10-14 10:25:53 +0200

    fix: admin watcher, ignore jwt errors and clear cache after boot

--- krme/shopware-platform-tools/1.0/docker/boot_end.sh
+++ krme/shopware-platform-tools/1.0/docker/boot_end.sh
@@ -2,4 +2,6 @@
 
 vendor/bin/platform-tools misc:install_dev_tools
 
+sed -i 's/openBrowserWithUrl(`${proxyUrlEnv.origin}`);//g' /var/www/html/vendor/shopware/storefront/Resources/app/storefront/build/start-hot-reload.js
 sed -i s/APP_ENV=prod/APP_ENV=dev/g /var/www/html/.env
+rm -rf /var/www/html/var/cache/*
